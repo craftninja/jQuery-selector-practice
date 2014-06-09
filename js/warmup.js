26b87b9298e80bde7f42198c0bbc42a34c4e638f
EmilyPlatzer <emilyplatzer@gmail.com>
2014-06-09 09:35:49 -0600

    jQuery selectors warmup - 6/8 complete

--- js/warmup.js
+++ js/warmup.js
@@ -12,26 +12,26 @@ window.Warmup = {
       Description: show the names of the people at the second level
       Outcome: Joel, Aaliyah, Claude
     */
-    showNamesForSelector("#second-level-button", ___);
+    showNamesForSelector("#second-level-button", "#root > li > ul > li > span");
     //                       You need to replace ^^^ with a valid CSS selector
 
     /*
       Description: show the names of the people who have data-employment="full-time"
       Outcome: Izaiah, Della, Lowell
     */
-    showNamesForSelector("#full-time-button", ___);
+    showNamesForSelector("#full-time-button", "[data-employment=full-time]");
 
     /*
       Description: show the names of the people with the class name "talkative"
       Outcome: Claude, Lurline
     */
-    showNamesForSelector("#talkative-button", ___);
+    showNamesForSelector("#talkative-button", ".talkative");
 
     /*
       Description: show the names of the people who have an id of "vp"
       Outcome: Eulalia
     */
-    showNamesForSelector("#vp-button", ___);
+    showNamesForSelector("#vp-button", '#vp');
 
     /*
       Description: show the names of the people under Isaiah
@@ -49,7 +49,7 @@ window.Warmup = {
       Description: values of all of the input fields of type text
       Outcome: Joe, Example
     */
-    showValuesForSelector("#text-field-button", ___);
+    showValuesForSelector("#text-field-button", ':text');
 
   }
 
